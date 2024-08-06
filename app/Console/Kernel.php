f0c70cc1bcd9742ebef6a8db333ff882d85d9d96
rezaandreannn <andreanreza042@gmail.com>
2024-08-06 14:54:40 +0700

    add log schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use Log;
+use App\Models\ScheduleLog;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -18,31 +21,82 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         // assign data pasien terdaftar
         $schedule->command('patient:assign-id-satusehat')
-            ->everyFiveMinutes()
-            ->timezone('Asia/Jakarta');
+            ->everyFifteenMinutes()
+            ->timezone('Asia/Jakarta')
+            ->onSuccess(function () {
+                // Simpan waktu eksekusi terakhir ke database
+                ScheduleLog::updateOrInsert(
+                    [
+                        'command_name' => 'patient:assign-id-satusehat',
+                    ],
+                    [
+                        'last_run_at' => now(),
+                        'description' => 'post data pasien ke satu sehat menggunakan nik'
+                    ]
+                );
+            });
         // send kedatangan pasien
         $schedule->command('encounter:rajal')
             ->hourly()
             ->timezone('Asia/Jakarta')
-            ->between('8:00', '20:00');
+            ->between('8:00', '20:00')
+            ->onSuccess(function () {
+                // Simpan waktu eksekusi terakhir ke database
+                ScheduleLog::updateOrInsert(
+                    ['command_name' => 'encounter:rajal'],
+                    [
+                        'last_run_at' => now(),
+                        'description' => 'post data kunjungan pasien yang telah datang'
+                    ]
+                );
+            });
 
         // send ttv pasien
         $schedule->command('observation:send')
             ->hourly()
             ->timezone('Asia/Jakarta')
-            ->between('8:00', '20:00');
+            ->between('8:00', '20:00')
+            ->onSuccess(function () {
+                // Simpan waktu eksekusi terakhir ke database
+                ScheduleLog::updateOrInsert(
+                    ['command_name' => 'observation:send'],
+                    [
+                        'last_run_at' => now(),
+                        'description' => 'post data TTV nadi pasien yang telah di assesmen oleh perawat'
+                    ]
+                );
+            });
 
         // send condition 
         $schedule->command('condition:send')
             ->hourly()
             ->timezone('Asia/Jakarta')
-            ->between('8:00', '20:00');
-
+            ->between('8:00', '20:00')
+            ->onSuccess(function () {
+                // Simpan waktu eksekusi terakhir ke database
+                ScheduleLog::updateOrInsert(
+                    ['command_name' => 'condition:send'],
+                    [
+                        'last_run_at' => now(),
+                        'description' => 'post data condition atau diagnosa yang telah diinput oleh petugas RM'
+                    ]
+                );
+            });
         // update finished encouter
         $schedule->command('update:encounter')
             ->hourly()
             ->timezone('Asia/Jakarta')
-            ->between('8:00', '20:00');
+            ->between('8:00', '20:00')
+            ->after(function () {
+                // Simpan waktu eksekusi terakhir ke database
+                ScheduleLog::updateOrInsert(
+                    ['command_name' => 'update:encounter'],
+                    [
+                        'last_run_at' => now(),
+                        'description' => 'update data kunjungan yang telah di input diagnosanya oleh petugas RM, rubah status nya menjadi selesai'
+                    ]
+                );
+            });
     }
 
     /**
---
+++ database/migrations/2024_08_06_105750_create_schedule_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('command_name')->unique();
+            $table->timestamp('last_run_at')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule_logs');
+    }
+};
